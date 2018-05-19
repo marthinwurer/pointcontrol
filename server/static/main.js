24f2a0adb11ec320940ef00c08a2abfce88abd58
Benjamin Maitland <marthinwurer@gmail.com>
2018-05-19 14:30:22 -0400

    Fixed it! The script had to be after the body for some reason.

--- server/static/main.js
+++ server/static/main.js
@@ -1,67 +1,65 @@
-
-
 currentId = 0;
 
 function getData(id) {
-  if (id == 0) {
-    return;
-  }
-  weapon = $('input[name=optionsWeapons]:checked', '#fencerform').val();
-  $(".loading-image").show();
-  $.getJSON("get?id="+id+"&weapon="+weapon,
-      function(result) {
-        $.getJSON("translate?id="+id , function (res) {
-          name = res["name"]
-          currentId = id;
-          drawBasic(result, name);
-          $(".loading-image").hide();
-        });
-      }
-  );
+    if (id === 0) {
+        return;
+    }
+    weapon = $('input[name=optionsWeapons]:checked', '#fencerform').val();
+    $(".loading-image").show();
+    $.getJSON("get?id=" + id + "&weapon=" + weapon,
+        function (result) {
+            $.getJSON("translate?id=" + id, function (res) {
+                var fencer_name = res["name"];
+                currentId = id;
+                drawBasic(result, fencer_name);
+                $(".loading-image").hide();
+            });
+        }
+    );
 }
 
 function queryEvents(q) {
-  $.getJSON("event?q="+q,
-      function(result) {
-          displayEvents(result);
-          $(".loading-image").hide();
-      }
-  );
+    $.getJSON("event?q=" + q,
+        function (result) {
+            displayEvents(result);
+            $(".loading-image").hide();
+        }
+    );
 }
 
 function rateEvent(tournament_id, event_id) {
-  $.getJSON("rate_event?tournament_id="+tournament_id+"&event_id="+event_id,
-      function(ratings) {
-        for (i = 0 ; i < ratings.length; i++) {
-            $("#title_box").append('<h3>' + ratings[i]["name"] + ' - ' + ratings[i]["birthyear"] + ' - ' + ratings[i]["rating"] + "</h3>");
+    $.getJSON("rate_event?tournament_id=" + tournament_id + "&event_id=" + event_id,
+        function (ratings) {
+            for (i = 0; i < ratings.length; i++) {
+                $("#title_box").append('<h3>' + ratings[i]["name"] + ' - ' + ratings[i]["birthyear"] + ' - ' + ratings[i]["rating"] + "</h3>");
+            }
+            $(".loading-image").hide();
         }
-        $(".loading-image").hide();
-      }
-  );
+    );
 }
 
 function drawBasic(fencer, fullname) {
-      // 116580
-      dateseen = {}
-      rows = [];
-      fencer_name = fullname;
-      console.log(fencer_name);
-      if (fencer != 0) {
+    // 116580
+    dateseen = {};
+    rows = [];
+    fencer_name = fullname;
+    console.log(fencer_name);
+    if (fencer != 0) {
         for (i = 0; i < fencer['ratings'].length; i++) {
-          if (fencer['ratings'][i]['date'] in dateseen)
-            continue;
-          dateseen[fencer['ratings'][i]['date']] = 1;
-          rows.push([Date.parse(fencer['ratings'][i]['date']), fencer['ratings'][i]['rating']]);
+            if (fencer['ratings'][i]['date'] in dateseen)
+                continue;
+            dateseen[fencer['ratings'][i]['date']] = 1;
+            rows.push([Date.parse(fencer['ratings'][i]['date']), fencer['ratings'][i]['rating']]);
         }
-      }
-      latest_rank = 0;
-      if (rows.length != 0) {
-        latest_rank = rows[rows.length - 1][1] 
-      }
+    }
+    latest_rank = 0;
+    if (rows.length != 0) {
+        latest_rank = rows[rows.length - 1][1]
+    }
     $('#container').highcharts({
 
         credits: {
-             enabled: false
+            enabled: false
         },
         chart: {
             type: 'line'
@@ -86,7 +84,7 @@ function drawBasic(fencer, fullname) {
         yAxis: {
             title: {
                 text: 'Rating'
-            },
+            }
         },
         tooltip: {
             headerFormat: '<b>{series.name}</b><br>',
@@ -95,14 +93,14 @@ function drawBasic(fencer, fullname) {
 
         plotOptions: {
             series: {
-                cursor:'pointer',
+                cursor: 'pointer',
                 marker: {
                     enabled: true
                 },
                 point: {
                     events: {
                         click: function (e) {
-                          console.log(this);
+                            console.log(this);
                         }
                     }
                 }
@@ -117,13 +115,14 @@ function drawBasic(fencer, fullname) {
             data: rows
         }]
     });
-} 
+}
+
 function drawFrame() {
-    rows = []
+    rows = [];
     $('#container').highcharts({
 
         credits: {
-             enabled: false
+            enabled: false
         },
         chart: {
             type: 'line'
@@ -145,7 +144,7 @@ function drawFrame() {
         yAxis: {
             title: {
                 text: 'Rating'
-            },
+            }
         },
         tooltip: {
             headerFormat: '<b>{series.name}</b><br>',
@@ -154,10 +153,10 @@ function drawFrame() {
 
         plotOptions: {
             series: {
-                cursor:'pointer',
+                cursor: 'pointer',
                 marker: {
                     enabled: true
-                },
+                }
             }
         },
 
@@ -173,70 +172,70 @@ function drawFrame() {
 }
 
 function displayEvents(events) {
-  $("#title_thing").text("Click on the correct event...");
-  $("#subtitle_thing").text("Tournament - Event - Start Date");
-  $( "h3" ).empty();
-  for (i = 0 ; i < events.length; i++) {
-    $("#title_box").append('<h3 tournament_id="'+events[i]["tournament_id"]+'" event_id="' +events[i]["event_id"]+ '" tename="' + events[i]["tname"] + ' ' + events[i]["ename"]  + '">' + events[i]["tname"] + ' - ' + events[i]["ename"] + ' - ' + events[i]["start_date"] + "</h3>");
-  }
-  $("h3").click(function() {
-      $(".loading-image").show();
-      $("#title_thing").text($(this).attr('tename'));
-      $("#subtitle_thing").text("Name - Birthyear - Latest Rating");
-      $("h3").empty()
-      ratings = rateEvent($(this).attr('tournament_id'), $(this).attr('event_id'));
-  });
+    $("#title_thing").text("Click on the correct event...");
+    $("#subtitle_thing").text("Tournament - Event - Start Date");
+    $("h3").empty();
+    for (i = 0; i < events.length; i++) {
+        $("#title_box").append('<h3 tournament_id="' + events[i]["tournament_id"] + '" event_id="' + events[i]["event_id"] + '" tename="' + events[i]["tname"] + ' ' + events[i]["ename"] + '">' + events[i]["tname"] + ' - ' + events[i]["ename"] + ' - ' + events[i]["start_date"] + "</h3>");
+    }
+    $("h3").click(function () {
+        $(".loading-image").show();
+        $("#title_thing").text($(this).attr('tename'));
+        $("#subtitle_thing").text("Name - Birthyear - Latest Rating");
+        $("h3").empty()
+        ratings = rateEvent($(this).attr('tournament_id'), $(this).attr('event_id'));
+    });
 }
 
 function displayNames(names) {
-  $("#title_thing").text("Click on the correct fencer...");
-  $("#subtitle_thing").text("Full Name - Birth Year - Last Tournament");
-  $( "h3" ).empty();
-  for (i = 0 ; i < names.length; i++) {
-    $("#title_box").append('<h3 id="'+names[i]["id"]+'">' + names[i]["fullname"] + ' - ' + names[i]["birthyear"] + ' - ' + names[i]["t_date"]+ "</h3>");
-  }
-  $("h3").click(function() {
-    getData($(this).attr('id'));
-  });
+    $("#title_thing").text("Click on the correct fencer...");
+    $("#subtitle_thing").text("Full Name - Birth Year - Last Tournament");
+    $("h3").empty();
+    for (i = 0; i < names.length; i++) {
+        $("#title_box").append('<h3 id="' + names[i]["id"] + '">' + names[i]["fullname"] + ' - ' + names[i]["birthyear"] + ' - ' + names[i]["t_date"] + "</h3>");
+    }
+    $("h3").click(function () {
+        getData($(this).attr('id'));
+    });
 }
 
 function displayMore(names) {
 }
 
 
-$( document ).ready(function() {
+$(document).ready(function () {
     drawFrame();
-  $(".pure-radio").click(function() {
-    getData(currentId);
-  });
+    $(".pure-radio").click(function () {
+        getData(currentId);
+    });
 });
-$( "#fencerform" ).submit(function( event ) {
-  console.log(event);
-  value_string = $('#fenceridinput').val();
-  if (!isNaN(value_string)) {
-    getData(parseInt(value_string));
-  } else {
-    value_name = $.trim(value_string);
-  $(".loading-image").show();
-    $.getJSON("name?q="+value_name,
-        function(result) {
-          console.log(result);
-          displayNames(result);
-          $(".loading-image").hide();
-        }
-    );
-  } 
-  event.preventDefault();
+$("#fencerform").submit(function (event) {
+    console.log(event);
+    value_string = $('#fenceridinput').val();
+    if (!isNaN(value_string)) {
+        getData(parseInt(value_string));
+    } else {
+        value_name = $.trim(value_string);
+        $(".loading-image").show();
+        $.getJSON("name?q=" + value_name,
+            function (result) {
+                console.log(result);
+                displayNames(result);
+                $(".loading-image").hide();
+            }
+        );
+    }
+    event.preventDefault();
 });
 
-$( "#eventform" ).submit(function( event ) {
-  value_string = $('#eventquery').val();
-  if (!isNaN(value_string)) {
-    //console.log("hi")
-    //queryEvents(value_string);
-  } else {
-    $(".loading-image").show();
-    queryEvents(value_string);
-  } 
-  event.preventDefault();
+$("#eventform").submit(function (event) {
+    value_string = $('#eventquery').val();
+    if (!isNaN(value_string)) {
+        //console.log("hi")
+        //queryEvents(value_string);
+    } else {
+        $(".loading-image").show();
+        queryEvents(value_string);
+    }
+    event.preventDefault();
 });
