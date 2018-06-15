615969eb1e90f822241d70e861c23f0f0f525dbb
Benjamin Maitland <marthinwurer@gmail.com>
2018-06-15 07:38:25 -0400

    Fixed whitespace in rate.py

--- rate.py
+++ rate.py
@@ -3,17 +3,18 @@ import argparse
 import trueskill
 from datetime import datetime, timedelta
 
+# def main():
 parser = argparse.ArgumentParser(description='rate.py uses Trueskill to rank fencers')
 parser.add_argument("-d", action="store", dest="db", default="data.db",
-    help="Specify target db")
+                    help="Specify target db")
 parser.add_argument("--weapon", action="store", dest="weapon", default=None,
-    help="Specify weapon")
+                    help="Specify weapon")
 parser.add_argument("--days", action="store", dest="scrape_lookback", default=None,
-    help="Scrape lookback in calendar days")
+                    help="Scrape lookback in calendar days")
 parser.add_argument("--begin-date", action="store", dest="begin_date", default=None,
-    help="Scrape begin date in yyyy-mm-dd")
+                    help="Scrape begin date in yyyy-mm-dd")
 parser.add_argument("--end-date", action="store", dest="end_date", default=datetime.now().isoformat().split("T")[0],
-    help="Scrape end date in yyyy-mm-dd")
+                    help="Scrape end date in yyyy-mm-dd")
 
 args = parser.parse_args()
 
@@ -23,19 +24,20 @@ c = conn.cursor()
 end_date = datetime.strptime(args.end_date, '%Y-%m-%d')
 
 if args.begin_date is not None:
-  begin_date = datetime.strptime(args.begin_date, '%Y-%m-%d')
+    begin_date = datetime.strptime(args.begin_date, '%Y-%m-%d')
 elif args.scrape_lookback is not None:
-  begin_date = datetime.now() - timedelta(days=int(args.scrape_lookback))
+    begin_date = datetime.now() - timedelta(days=int(args.scrape_lookback))
 
 trueskill.setup(draw_probability=0.0018469)
 
 FENCER_STATE = dict()
 
+
 def updateRank(bout):
-  def getLatestRating(fencerid, weapon):
-    if (fencerid, weapon) in FENCER_STATE:
-      return FENCER_STATE[(fencerid, weapon)]
-    query = c.execute("""
+    def getLatestRating(fencerid, weapon):
+        if (fencerid, weapon) in FENCER_STATE:
+            return FENCER_STATE[(fencerid, weapon)]
+        query = c.execute("""
       SELECT r.ts_mu, r.ts_sigma FROM ratings r , bouts b, events e, tournaments t
       WHERE r.fencerid = %(fencerid)s
         AND r.boutid = b.boutid
@@ -46,62 +48,62 @@ def updateRank(bout):
       ORDER BY t.start_date desc, b.boutid desc
       LIMIT 1;
       """ % {
-        "fencerid" : fencerid,
-        "weapon" : weapon,
-        "boutid" : boutid,
-        "begin_date" : begin_date
+            "fencerid": fencerid,
+            "weapon": weapon,
+            "boutid": boutid,
+            "begin_date": begin_date
         }).fetchone()
-    if query:
-      return query
-    else:
-      return (trueskill.MU, trueskill.SIGMA)
-  def updateLatestRating(fencerid, weapon, boutid, rating, prev_rating):
-    query = c.execute("""
+        if query:
+            return query
+        else:
+            return (trueskill.MU, trueskill.SIGMA)
+
+    def updateLatestRating(fencerid, weapon, boutid, rating, prev_rating):
+        query = c.execute("""
       INSERT OR REPLACE INTO ratings
       (fencerid, weapon, boutid, ts_mu, ts_sigma, prev_ts_mu, prev_ts_sigma)
       VALUES
       (%(fencerid)s, '%(weapon)s', %(boutid)s, %(ts_mu)f, %(ts_sigma)f, %(prev_ts_mu)f, %(prev_ts_sigma)f)
       """ % {
-        "fencerid" : fencerid,
-        "weapon" : weapon,
-        "boutid" : boutid,
-        "ts_mu" : rating.mu,
-        "ts_sigma" : rating.sigma,
-        "prev_ts_mu" : prev_rating.mu,
-        "prev_ts_sigma" : prev_rating.sigma,
+            "fencerid": fencerid,
+            "weapon": weapon,
+            "boutid": boutid,
+            "ts_mu": rating.mu,
+            "ts_sigma": rating.sigma,
+            "prev_ts_mu": prev_rating.mu,
+            "prev_ts_sigma": prev_rating.sigma,
         })
-    FENCER_STATE[(fencerid, weapon)] = (rating.mu, rating.sigma)
+        FENCER_STATE[(fencerid, weapon)] = (rating.mu, rating.sigma)
 
-  boutid, fencer1id, fencer2id, score1, score2, de_or_pool, weapon, start_date = bout
-  oldp1 = getLatestRating(fencer1id, weapon)
-  oldp1 = trueskill.Rating(oldp1[0], oldp1[1])
-  oldp2 = getLatestRating(fencer2id, weapon)
-  oldp2 = trueskill.Rating(oldp2[0], oldp2[1])
-  print (oldp1, oldp2)
+    boutid, fencer1id, fencer2id, score1, score2, de_or_pool, weapon, start_date = bout
+    oldp1 = getLatestRating(fencer1id, weapon)
+    oldp1 = trueskill.Rating(oldp1[0], oldp1[1])
+    oldp2 = getLatestRating(fencer2id, weapon)
+    oldp2 = trueskill.Rating(oldp2[0], oldp2[1])
+    print (oldp1, oldp2)
 
-  if de_or_pool == "pool":
-    if score1 > score2:
-      p1, p2 = trueskill.rate_1vs1(oldp1, oldp2)
-    elif score2 > score1:
-      p2, p1 = trueskill.rate_1vs1(oldp2, oldp1)
-    else:
-      p1, p2 = trueskill.rate_1vs1(oldp1, oldp2, drawn = True)
-  elif de_or_pool == "de":
-    # For now... DE's count double
-    if score1 > score2:
-      p1, p2 = trueskill.rate_1vs1(oldp1, oldp2)
-      p1, p2 = trueskill.rate_1vs1(oldp1, oldp2)
-    elif score2 > score1:
-      p2, p1 = trueskill.rate_1vs1(oldp2, oldp1)
-      p2, p1 = trueskill.rate_1vs1(oldp2, oldp1)
-    else:
-      p1, p2 = trueskill.rate_1vs1(oldp1, oldp2, drawn = True)
-      p1, p2 = trueskill.rate_1vs1(oldp1, oldp2, drawn = True)
+    if de_or_pool == "pool":
+        if score1 > score2:
+            p1, p2 = trueskill.rate_1vs1(oldp1, oldp2)
+        elif score2 > score1:
+            p2, p1 = trueskill.rate_1vs1(oldp2, oldp1)
+        else:
+            p1, p2 = trueskill.rate_1vs1(oldp1, oldp2, drawn=True)
+    elif de_or_pool == "de":
+        # For now... DE's count double
+        if score1 > score2:
+            p1, p2 = trueskill.rate_1vs1(oldp1, oldp2)
+            p1, p2 = trueskill.rate_1vs1(oldp1, oldp2)
+        elif score2 > score1:
+            p2, p1 = trueskill.rate_1vs1(oldp2, oldp1)
+            p2, p1 = trueskill.rate_1vs1(oldp2, oldp1)
+        else:
+            p1, p2 = trueskill.rate_1vs1(oldp1, oldp2, drawn=True)
+            p1, p2 = trueskill.rate_1vs1(oldp1, oldp2, drawn=True)
 
-  updateLatestRating(fencer1id, weapon, boutid, p1, oldp1)
-  updateLatestRating(fencer2id, weapon, boutid, p2, oldp2)
-  print (p1, p2)
-
+    updateLatestRating(fencer1id, weapon, boutid, p1, oldp1)
+    updateLatestRating(fencer2id, weapon, boutid, p2, oldp2)
+    print (p1, p2)
 
 
 c_bouts = conn.cursor()
@@ -123,12 +125,16 @@ rows = c_bouts.execute("""
       AND t.start_date <= '%(end_date)s'
     ORDER BY t.start_date asc, b.boutid asc
     """ % {
-      "begin_date" : begin_date,
-      "end_date" : end_date,
-      "weapon" : args.weapon,
-    })
+    "begin_date": begin_date,
+    "end_date": end_date,
+    "weapon": args.weapon,
+})
 
 for row in rows:
-  print (row)
-  updateRank(row)
+    print (row)
+    updateRank(row)
 conn.commit()
+
+# if __name__ == "__main__":
+#     main()
+
