0ac3380bdceb451173f59a8862f66ed5f220e4d7
Benjamin Maitland <marthinwurer@gmail.com>
2020-01-09 03:55:25 -0500

    Got individual ratings working

--- flask_server/main.py
+++ flask_server/main.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import sqlite3
 import sys
 
@@ -12,6 +13,8 @@ app = Flask(__name__,
 
 
 def _execute(query, args):
+    if not isinstance(args, (tuple, list)):
+        args = (args,)
     dbPath = "../data_for_ben.db"
     connection = sqlite3.connect(dbPath)
     cursorobj = connection.cursor()
@@ -34,8 +37,8 @@ def index():
 def search():
     name = request.args.get("q")
 
-    query = ''' select first_name || " " || last_name, f.fencerid, f.birthyear
-            from fencers f
+    query = ''' select f.first_name || " " || f.last_name, f.fencerid, f.birthyear
+            from fencers as f
             where f.first_name || " " || f.last_name like "%"||(?)||"%"
             group by f.fencerid
             '''
@@ -72,7 +75,24 @@ def get_rating():
         data_points["ratings"].append(point)
     return json.dumps(data_points)
 
+
+@app.route("/translate")
+def translate():
+    fencerid = int(request.args.get("id"))
+    query = """
+        select first_name || " " || last_name 
+        from fencers
+        where fencerid = (?)
+    """
+    obj = {"name": _execute(query, (fencerid,))[0]}
+    return json.dumps(obj)
+
+
 if __name__ == '__main__':
     db_name = sys.argv[1]
-    server = WSGIServer(('127.0.0.1', 5000), app)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler("server.log")
+    logger.addHandler(handler)
+    server = WSGIServer(('127.0.0.1', 5000), app, log=logger, error_log=logger)
     server.serve_forever()
